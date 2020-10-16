03554d47735668a18032e5a90afc96c9bf12eb70
Pepijn de Kruijff <pepijn4@icloud.com>
2020-10-16 16:16:03 +0200

    button

--- fit4u/app/src/main/java/com/example/myapplication/MainActivity.java
+++ fit4u/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -25,6 +29,16 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+    }
+
+
+
+
+    public void buttonClicked(View view) {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        navDirections action = SpecifyAmountFragmentDirections
+//                .actionSpecifyAmountFragmentToConfirmationFragment();
+
     }
 
 }
--- fit4u/app/src/main/java/com/example/myapplication/TrainerProfileFragment.java
+++ fit4u/app/src/main/java/com/example/myapplication/TrainerProfileFragment.java
@@ -12,18 +12,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class trainerProfile extends Fragment {
+public class TrainerProfileFragment extends Fragment {
 
     private TrainerProfileViewModel mViewModel;
 
-    public static trainerProfile newInstance() {
-        return new trainerProfile();
+    public static TrainerProfileFragment newInstance() {
+        return new TrainerProfileFragment();
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.trainer_profile_fragment, container, false);
+        return inflater.inflate(R.layout.fragment_trainer_profile, container, false);
     }
 
     @Override
